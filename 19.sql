a8cb93c7ee562528e4e7ee690fb9d62a43ebbb87
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-08-31 19:26:33 -0700

    mười chín.sql

---
+++ 19.sql
@@ -0,0 +1,10 @@
+SELECT Employee.FirstName, Employee.LastName, count(*)
+FROM Employee
+JOIN Customer
+ON Customer.SupportRepId = Employee.EmployeeId
+JOIN Invoice
+ON Invoice.CustomerId = Customer.CustomerId
+WHERE Invoice.InvoiceDate LIKE '%2009%'
+GROUP BY Employee.EmployeeId
+ORDER BY count(*) desc
+LIMIT 1
