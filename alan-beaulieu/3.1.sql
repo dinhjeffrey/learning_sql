dfaaac6bc782d81a5cb9871d38e879d337a3d635
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-09-05 21:39:37 -0700

    added 3.1

---
+++ alan-beaulieu/3.1.sql
@@ -0,0 +1,5 @@
+-- Retrieve the employee ID, first name, and last name for all bank employees. Sort by last name and then by first name.
+
+select emp_id, fname, lname
+from employee
+order by lname, fname;
