a2026c33cb1c95f7e4847442794da818b8780a58
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-09-06 10:57:36 -0700

    added 3.2

---
+++ alan-beaulieu/3.2.sql
@@ -0,0 +1,3 @@
+-- Retrieve the account ID, customer ID, and available balance for all accounts whose status equals 'ACTIVE' and whose available balance is greater than $2,500.
+
+select account_id, cust_id, balance
