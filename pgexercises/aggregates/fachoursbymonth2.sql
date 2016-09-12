3c4de5947eb76732d95a3b143b03b886ccc1b94c
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-09-11 18:19:22 -0700

    added fachoursbymonth2

---
+++ pgexercises/aggregates/fachoursbymonth2.sql
@@ -0,0 +1,10 @@
+-- Produce a list of the total number of slots booked per facility per month in the year of 2012. Produce an output table consisting of facility id and slots, sorted by the id and month.
+
+select facid,
+extract(month from starttime) "month",
+sum(slots) "Total Slots"
+from cd.bookings
+where starttime >= '2012-01-01'
+and starttime < '2013-01-01'
+group by facid, extract(month from starttime)
+order by facid, extract(month from starttime);
